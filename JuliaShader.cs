359dcb3888e5689b1e0d4fb37854023751fae562
Iskander Biktashev <iskanderbikta@yandex.ru>
2025-03-26 22:21:31 +0300

    удалил для множества жюлиа бесполезную проверку на принадлежность кардиоиде

--- JuliaShader.cs
+++ JuliaShader.cs
@@ -17,16 +17,6 @@ namespace FractalViewer
         {   
             double2 z = new((ThreadIds.X - size.X * 0.5f) / scale + offset.X, (ThreadIds.Y - size.Y * 0.5f) / scale + offset.Y);
 
-            // Проверка на принадлежность точки главной кардиоиде
-            var rho = Hlsl.Sqrt(Hlsl.Pow((float)c.X - .25f, 2) + (float)c.Y * (float)c.Y);
-            var theta = Hlsl.Atan2((float)c.Y, (float)c.X - .25f);
-
-            if (rho <= .5f - .5f * Hlsl.Cos(theta))
-            {
-                texture[ThreadIds.XY].RGBA = new(0, 0, 0, 1);
-                return;
-            }
-
             // Основной цикл
             for (int i = 0; i < iterations; i++)
             {
