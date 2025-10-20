4c8f07383ff4b88c5304ce7ab1e1b4e283313274
iskanye <iskanyeban@gmail.com>
2025-10-20 15:16:00 +0300

    feat: added old window

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,9 +1,27 @@
+using Avalonia;
 using Avalonia.Controls;
+using ComputeSharp;
 
 namespace FractalViewer;
 
 public partial class MainWindow : Window
 {
+    private readonly GraphicsDevice gpu = GraphicsDevice.GetDefault();
+
+    private double2 offset;
+    private double scale = 300;
+    
+    private int Iterations
+    {
+        get => (int)iterationsSlider.Value;
+    }
+    private int2 Size
+    {
+        get => WindowState == WindowState.Maximized ? new((int)Width, (int)Height) : new((int)Width, (int)Height);
+    }
+    private bool isJulia;
+    private Point startPos;
+
     public MainWindow()
     {
         InitializeComponent();
